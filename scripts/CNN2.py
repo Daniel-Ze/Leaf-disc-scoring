095d230c09f05cd8016e7a46e5b2264ced1ff259
Daniel <d.zendler@gmx.de>
2021-03-17 13:57:20 +0100

    .

--- scripts/CNN2.py
+++ scripts/CNN2.py
@@ -40,10 +40,10 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(512))
+model.add(Dense(1024))
 model.add(Activation('relu'))
 model.add(Dropout(dropout))
-model.add(Dense(512))
+model.add(Dense(1024))
 model.add(Activation('relu'))
 model.add(Dropout(dropout))
 model.add(Dense(1))
