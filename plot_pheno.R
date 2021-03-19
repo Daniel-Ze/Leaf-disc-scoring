441e4401a95494a8d052f14283bada39b306ec3b
Daniel-Ze <d.zendler@gmx.de>
2021-03-19 10:31:13 +0100

    Update plot_pheno.R

--- plot_pheno.R
+++ plot_pheno.R
@@ -1,7 +1,10 @@
-#!/usr/bin/Rscript
+#!/usr/bin/env Rscript
 library(ggplot2)
 library(readr)
 library(R.utils)
+
+# version 0.1
+# license http://creativecommons.org/licenses/by-nc-sa/4.0/
 
 results <- cmdArg("r")
 
@@ -10,17 +13,19 @@ results_file_name <- basename( results )
 
 results_tbl <- read.table( results,
                            header = TRUE,
-                           sep = "\t")
+                           sep = "\t",
+                           comment.char = "#"
+                         )
 
 ggplot(results_tbl, aes(x=Exp_name,y=perc_spo))+
-     geom_violin()+
+     geom_violin(alpha = 0.6)+
      geom_boxplot(width = 0.2, alpha = 0.5)+
-     geom_jitter(width = 0.2)+
+     geom_jitter(width = 0.1, height = 0.01)+
      theme_bw()+
      theme(axis.title.x = element_blank())+
      ylab("% leaf with sporangia")+
      ggtitle("Phenotypic score distribution")
 
 ggsave(paste0(results_file_path,"/results/pheno_score_plot.png"),
-      width = 4,
-      height = 4)
+      width = 3,
+      height = 5)
